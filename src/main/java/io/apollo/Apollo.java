773815ef8cd0cb97b4820afffe4a52754f6faf55
pinkulu <56201697+pinkulu@users.noreply.github.com>
2020-09-08 23:55:08 +0300

    New moduels, and more
    Added DiscordRP and a DiscordRP module, also there's a WindowedFullscreen module.
    Made an example folder and put examples for Module and Slothpixel
    Updated the readme

--- src/main/java/io/apollo/Apollo.java
+++ src/main/java/io/apollo/Apollo.java
@@ -1,24 +1,21 @@
 /*⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤
  Copyright (C) 2020-2021 developed by Icovid and Apollo Development Team
-
  This program is free software: you can redistribute it and/or modify
  it under the terms of the GNU Affero General Public License as published
  by the Free Software Foundation, either version 3 of the License, or
  (at your option) any later version.
-
  This program is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU Affero General Public License for more details.
-
  You should have received a copy of the GNU Affero General Public License
  along with this program.  If not, see https://www.gnu.org/licenses/.
-
  Contact: Icovid#3888 @ https://discord.com
  ⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤*/
 
 package io.apollo;
 
+import io.apollo.discord.DiscordRP;
 import io.apollo.events.bus.EventBus;
 import io.apollo.modules.ModuleManager;
 
@@ -30,7 +27,7 @@ import java.util.Date;
  * @author Icovid | Icovid#3888
  * @since 1.0.0 **/
 public class Apollo {
-
+    public static DiscordRP discordRP = new DiscordRP();
     // Apollo information used for display title and credits info.
     public static final String NAME = "Apollo", VERSION = "b0.1", MCVERSION = "1.8.9";
     public static final String[] DEVELOPERS = {"Icovid", "Nora", "isXander"};
@@ -52,10 +49,15 @@ public class Apollo {
     }
 
     /** Log Apollo instance stats after construction. **/
-    public void postInitialisation() { log("Apollo Initiation Finished with 0 Modules and 0 Settings! "); }
+    public void postInitialisation() { log("Apollo Initiation Finished with 0 Modules and 0 Settings! ");
+            //DiscordRP.Main("Test", "test2");
+        discordRP.start();
+        MODULE_MANAGER.preInitialisation();
+    }
 
     // Called when game shuts down.
     public void shutdown() {
+        discordRP.shutdown();
     }
 
     /** Used to log Apollo messages to console.
--- src/main/java/io/apollo/examples/SlothpixelExample.java
+++ src/main/java/io/apollo/examples/SlothpixelExample.java
@@ -1,28 +1,44 @@
 /*⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤
  Copyright (C) 2020-2021 developed by Icovid and Apollo Development Team
- 
+
  This program is free software: you can redistribute it and/or modify
  it under the terms of the GNU Affero General Public License as published
  by the Free Software Foundation, either version 3 of the License, or
  (at your option) any later version.
- 
+
  This program is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU Affero General Public License for more details.
- 
+
  You should have received a copy of the GNU Affero General Public License
  along with this program.  If not, see https://www.gnu.org/licenses/.
- 
+
  Contact: Icovid#3888 @ https://discord.com
  ⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤⏤*/
+package io.apollo.examples;
 
-package io.apollo.events.impl.client.input;
+import zone.nora.slothpixel.Slothpixel;
+import zone.nora.slothpixel.player.Player;
+import zone.nora.slothpixel.player.stats.bedwars.BedWars;
+import zone.nora.slothpixel.player.stats.tkr.Tkr;
 
-import io.apollo.events.Event;
+public class SlothpixelExample {
+    public static void main() {
+        // Make an instance of the Slothpixel API.
+        Slothpixel slothpixel = new Slothpixel();
+        // Save a request as a local variable.
+        Player player = slothpixel.getPlayer("Pinkulu");
 
-/** Fired when right mouse button is pressed.
- * @author Icovid | Icovid#3888
- * @since 1.0.0 **/
-public class RightClickEvent extends Event {
+        System.out.println("UUID: " + player.getUuid());
+        System.out.println("Karma: " + player.getKarma());
+        System.out.println("Discord: " + player.getLinks().getDiscord());
+
+        // Save a specific game's stats.
+        BedWars bedWars = player.getStats().getBedWars();
+
+        System.out.println("Coins: " + bedWars.getCoins());
+        System.out.println("Stars: " + bedWars.getLevel());
+        //Slothpixel DOCS: https://docs.slothpixel.me/
+    }
 }
