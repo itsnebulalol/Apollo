a57c0d6bd41d9627cc78f16f930e302881f93d0c
SLL <luisjk266@gmail.com>
2020-09-20 14:45:25 +0100

    Added GuiSwitchEvent by @MatthewTGM

---
+++ src/main/java/net/apolloclient/events/impl/client/GuiSwitchEvent.java
@@ -0,0 +1,18 @@
+package net.apolloclient.events.impl.client;
+
+import lombok.Getter;
+import net.apolloclient.events.Event;
+import net.minecraft.client.gui.GuiScreen;
+
+public class GuiSwitchEvent extends Event {
+
+    @Getter private GuiScreen guiScreen;
+
+    /**
+     * @param guiScreen the GuiScreen.
+     */
+    public GuiSwitchEvent(GuiScreen guiScreen)
+    {
+        this.guiScreen = guiScreen;
+    }
+}
